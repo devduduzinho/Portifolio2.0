d3fee4137ccf7ea22163dadb867d9cb07cf9e96b
eduardo <www.eduardo2005-12@hotmail.com>
2026-04-16 20:01:07 -0300

    feat: container

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,18 +1,44 @@
-import styled from "@emotion/styled"
+import { Grid, styled, Container, Typography, Button} from "@mui/material"
+import Avatar3 from "../../../../assets/images/avatar3.png"
 
 const Hero = () => {
-
     const StyledHero = styled("div")(() => ({
         backgroundColor: "black",
+        height: "100vh",
+        padding: "32px",
+        borderRadius: "24px",
+        color: "white",
     }))
 
-  return (
-    <>
+    const StyledImg = styled("img")(() => ({
+        width: "100%",
+        maxWidth: "300px",
+        borderRadius: "5%",
+        display: "block",
+        margin: "0 auto 24px",
+    }))
+
+    return (
+        <>
         <StyledHero>
-            ola
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid size={4}>
+                        <StyledImg src={Avatar3}/>
+                    </Grid>
+
+                <Grid size={8}>
+                    <Typography variant="h1">Eduardo Costa</Typography>
+                    <Typography variant="h2">Desenvolvedor Front-end</Typography>
+                    <Button>Download CV</Button>
+                    <Button>MEU Contato</Button>
+                </Grid>
+            </Grid>
+
+            </Container>
         </StyledHero>
-    </>
-  )
+        </>
+    )
 }
 
 export default Hero
